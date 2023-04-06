e71d6740dc7f303438d0cf39950bb3738606cb2b
Sergei Snitko <sergeisnit@yandex.ru>
2023-04-06 23:26:07 +0300

    DRF 3.2. Version 1

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,24 +1,35 @@
 from rest_framework import serializers
 
+from logistic.models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def validate(self, attrs):
+        return attrs
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
+        # print(positions)
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
@@ -26,6 +37,12 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            position.update({'stock': stock})
+            # print(position)
+            sp = StockProduct(**position)
+            sp.save()
+            # StockProduct().objects.create(position)
 
         return stock
 
@@ -39,5 +56,16 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            position.update({'stock': stock})
+            # for stock_product in StockProduct.objects.all():
+            #     # StockProduct.objects.update()
+            #     if stock_product.product == position['product']:
+            #         stock_product.quantity = position['quantity']
+            #         stock_product.price = position['price']
+            stock_product = StockProduct.objects.get(product=position['product'])
+            stock_product.quantity = position['quantity']
+            stock_product.price = position['price']
+            stock_product.save()
 
         return stock
